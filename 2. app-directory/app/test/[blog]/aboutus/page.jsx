05a9d671e865ee8d76f1d92189cba1b4af3ba942
kerimmstfdemir <kerimmstfdemir@gmail.com>
2023-03-24 09:54:57 +0300

    created page component

---
+++ 2. app-directory/app/test/[blog]/aboutus/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AboutUsPage = (props) => {
+  console.log("About Us Page \n-----------------------")
+  console.log(props)
+  return (
+    <div>About Us Page</div>
+  )
+}
+
+export default AboutUsPage
