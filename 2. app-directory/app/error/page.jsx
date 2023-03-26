6ae9d460f2426a0894762ae2f290416054389ca2
Kerim Mustafa Demir <kerimmstfdemir@gmail.com>
2023-03-26 14:49:46 +0300

    created not-found component

--- 2. app-directory/app/error/page.jsx
+++ 2. app-directory/app/error/page.jsx
@@ -1,6 +1,12 @@
+import { notFound } from "next/navigation" 
+
 const ErrorPage = ({ searchParams }) => {
     if (searchParams.error === "true") {
         throw new Error("This is an error!")
+    }
+
+    if ( searchParams.test === "true") {
+        notFound()
     }
     
     return (
---
+++ 2. app-directory/pages/404.jsx
@@ -0,0 +1,9 @@
+
+
+const Pages404 = () => {
+  return (
+    <h1>404 Not Found !!!</h1>
+  )
+}
+
+export default Pages404
