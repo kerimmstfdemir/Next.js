c9f6506d82732c7bdab93e4901ee366b1a3a9b6e
Kerim Mustafa Demir <kerimmstfdemir@gmail.com>
2023-03-26 16:09:34 +0300

    created head component

---
+++ 2. app-directory/app/aboutus/head.jsx
@@ -0,0 +1,7 @@
+const AboutUsHead = () => {
+  return (
+    <title>About Us Head</title>
+  )
+}
+
+export default AboutUsHead
---
+++ 2. app-directory/app/head.jsx
@@ -0,0 +1,10 @@
+const RootHead = () => {
+  return (
+    <>
+        <meta name="description" content="RootHead"/>
+        <title>Root Head</title>
+    </>
+  )
+}
+
+export default RootHead
